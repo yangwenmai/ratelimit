238c2f8c55df8a6a6b6a6bcfbf3d95270e666ee5
yangwenmai <yangwen.yw@gmail.com>
2017-06-08 09:30:15 +0800

    代码实现和增加测试用例

--- ratelimit.go
+++ ratelimit.go
@@ -1,15 +1,78 @@
 package ratelimit
 
+import (
+	"sync/atomic"
+	"time"
+)
+
 // RateLimiter 限速器
 type RateLimiter struct {
+	rate      uint64
+	allowance uint64
+	max       uint64
+	unit      uint64
+	lastCheck uint64
 }
 
 // New 创建RateLimiter实例
-func New() *RateLimiter {
-	return &RateLimiter{}
+func New(rate int, per time.Duration) *RateLimiter {
+	nano := uint64(per)
+	if nano < 1 {
+		nano = uint64(time.Second)
+	}
+	if rate < 1 {
+		rate = 1
+	}
+
+	return &RateLimiter{
+		rate:      uint64(rate),
+		allowance: uint64(rate) * nano,
+		max:       uint64(rate) * nano,
+		unit:      nano,
+
+		lastCheck: unixNano(),
+	}
 }
 
-// Limit 限制
+// Limit 判断是否超过限制
 func (rl *RateLimiter) Limit() bool {
+	now := unixNano()
+	// 计算上一次调用到现在过了多少纳秒
+	passed := now - atomic.SwapUint64(&rl.lastCheck, now)
+
+	rate := atomic.LoadUint64(&rl.rate)
+	current := atomic.AddUint64(&rl.allowance, passed*rate)
+
+	if max := atomic.LoadUint64(&rl.max); current > max {
+		atomic.AddUint64(&rl.allowance, max-current)
+		current = max
+	}
+
+	if current < rl.unit {
+		return true
+	}
+
+	// 没有超过限额
+	atomic.AddUint64(&rl.allowance, -rl.unit)
 	return false
+}
+
+// UpdateRate 更新速率值
+func (rl *RateLimiter) UpdateRate(rate int) {
+	atomic.StoreUint64(&rl.rate, uint64(rate))
+	atomic.StoreUint64(&rl.max, uint64(rate)*rl.unit)
+}
+
+// Undo 重置上一次调用Limit()，返回没有使用过的限额
+func (rl *RateLimiter) Undo() {
+	current := atomic.AddUint64(&rl.allowance, rl.unit)
+
+	if max := atomic.LoadUint64(&rl.max); current > max {
+		atomic.AddUint64(&rl.allowance, max-current)
+	}
+}
+
+// unixNano 当前时间（纳秒）
+func unixNano() uint64 {
+	return uint64(time.Now().UnixNano())
 }
--- ratelimit_test.go
+++ ratelimit_test.go
@@ -2,20 +2,38 @@ package ratelimit
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestLimit(t *testing.T) {
-	Convey("start test Limit(), result=false,", t, func() {
-		rl := New()
-		So(rl.Limit(), ShouldEqual, false)
+	Convey("初始化一个限制，然后进行测试限速: ", t, func() {
+		rl := New(1, time.Second)
+		for i := 0; i < 10; i++ {
+			if i == 0 {
+				So(rl.Limit(), ShouldEqual, false)
+			} else {
+				So(rl.Limit(), ShouldEqual, true)
+			}
+		}
+	})
+	Convey("第一次进行限速后，每次限速之前再重置，检查限速是否通过: ", t, func() {
+		rl := New(1, time.Second)
+		for i := 0; i < 10; i++ {
+			if i == 0 {
+				So(rl.Limit(), ShouldEqual, false)
+			} else {
+				rl.Undo()
+				So(rl.Limit(), ShouldEqual, false)
+			}
+		}
 	})
 }
 
 func BenchmarkLimit(b *testing.B) {
-	Convey("start BenchmarkLimit", b, func() {
-		rl := New()
+	Convey("开始压测限速", b, func() {
+		rl := New(1, time.Second)
 		for i := 0; i < b.N; i++ {
 			rl.Limit()
 		}
