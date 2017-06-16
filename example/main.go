af3179a66be087716bebadbab96e94158359ddd1
yangwenmai <yangwen.yw@gmail.com>
2017-06-16 10:49:39 +0800

    update doc & example

--- example/main.go
+++ example/main.go
@@ -9,16 +9,21 @@ import (
 )
 
 func main() {
+	// rate limit: simple
 	rl := simpleratelimit.New(10, time.Second)
 
 	for i := 0; i < 100; i++ {
-		rl.Limit()
+		log.Printf("limit result: %v\n", rl.Limit())
 	}
-	log.Printf("limit: %v", rl.Limit())
+	log.Printf("limit result: %v\n", rl.Limit())
+
+	// rate limit: leaky-bucket
 	lb := leakybucket.New()
 	b, err := lb.Create("leaky_bucket", 10, time.Second)
 	if err != nil {
 		log.Println(err)
 	}
-	log.Printf("capacity:%v", b.Capacity())
+	log.Printf("bucket capacity:%v", b.Capacity())
+
+	// rate limit: token-bucket
 }
